f41928b62afa96f052295158bb96aa1373123ee8
Bob <bobarke@gmail.com>
2026-03-23 10:24:31 -0700

    Update feeding time windows in DogFeedingTracker component for accuracy

--- app/page.tsx
+++ app/page.tsx
@@ -59,12 +59,12 @@ const DogFeedingTracker = () => {
 
       const breakfastEvents = todayEvents.filter(date => {
         const hour = date.getHours();
-        return hour >= 7 && hour < 10;
+        return hour >= 7 && hour < 12;
       });
 
       const dinnerEvents = todayEvents.filter(date => {
         const hour = date.getHours();
-        return hour >= 16 && hour < 20;
+        return hour >= 16 && hour < 22;
       });
 
       setBreakfastFed(breakfastEvents.length > 0);
@@ -214,13 +214,13 @@ const DogFeedingTracker = () => {
             meal="Breakfast"
             fed={breakfastFed}
             time={breakfastTime}
-            window="7:00 AM - 10:00 AM"
+            window="7:00 AM - 12:00 PM"
           />
           <MealCard
             meal="Dinner"
             fed={dinnerFed}
             time={dinnerTime}
-            window="4:00 PM - 8:00 PM"
+            window="4:00 PM - 10:00 PM"
           />
         </div>
 
